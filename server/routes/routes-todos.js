aeebcb65ea50cce9de87f082d66a0b2a08346f86
AbdulRehman197 <abdulrehman19761@gmail.com>
2018-09-18 01:56:19 -0700

    Server Compiled to Js

--- server/routes/routes-todos.js
+++ server/routes/routes-todos.js
@@ -25,7 +25,7 @@ router.post('/', auth_required_1.default, function (req, res) {
         return res.send(res.t);
     }
     let todo = new model_todos_1.default({
-        authorId: req.user._id,
+        // authorId: req.user._id,
         title: req.body.title.trim(),
         place: req.body.place.trim(),
         description: req.body.description.trim(),
@@ -43,6 +43,39 @@ router.get('/:id', auth_required_1.default, function (req, res) {
         }
         res.t.success = true;
         res.t.message = "Todo Found";
+        res.t.data = todo;
+        return res.send(res.t);
+    });
+});
+// Update Todos 
+router.put('/:id', auth_required_1.default, function (req, res) {
+    if (!req.body.title || !req.body.place || !req.body.description) {
+        res.t.message = "Invalid Request";
+        return res.send(res.t);
+    }
+    let { id } = req.params;
+    let { title, place, description } = req.body;
+    model_todos_1.default.findByIdAndUpdate(id, { title, place, description }).exec(function (err, todo) {
+        if (!todo) {
+            res.t.message = "Todo not available";
+            return res.send(res.t);
+        }
+        res.t.success = true;
+        res.t.message = "Todo Found";
+        res.t.data = todo;
+        return res.send(res.t);
+    });
+});
+//Delete Todo
+router.delete('/:id', auth_required_1.default, function (req, res) {
+    let { id } = req.params;
+    model_todos_1.default.findByIdAndRemove(id).exec(function (err, todo) {
+        if (!todo) {
+            res.t.message = "Todo not available";
+            return res.send(res.t);
+        }
+        res.t.success = true;
+        res.t.message = "Todo Deleted";
         res.t.data = todo;
         return res.send(res.t);
     });
