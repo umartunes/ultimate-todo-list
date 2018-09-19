4dee48fdd43426b42d0fe1b2ed7479139c93cc59
Umar Ahmad <s.umarahmad@gmail.com>
2018-09-19 23:42:31 +0500

    1st test written and failed

--- Part1-Step1-NoSQL/lib/config/db-config.ts
+++ Part1-Step1-NoSQL/lib/config/db-config.ts
@@ -1,8 +1,6 @@
-
-import * as mongoose from 'mongoose';
+import mongoose = require('mongoose');
 
 var db_url = process.env.DB_URL || 'mongodb://localhost:27017/ultimate-todo'
-mongoose.set('useFindAndModify', false)
 mongoose.connect(db_url, { useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
---
+++ Part1-Step1-NoSQL/lib/test/api-todos.test.ts
@@ -0,0 +1,12 @@
+import * as request from "supertest";
+import server from '../app';
+
+
+//Test either todos are coming or not
+describe("GET todos list test", () => {
+  it("should return 200 OK", () => {
+    return request(server).get("/api/todos/")
+      .expect(200);
+  });
+});
+
