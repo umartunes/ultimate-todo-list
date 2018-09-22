81306bb746b711567c528d7bbe59c979bc254db9
AbdulRehman197 <abdulrehman19761@gmail.com>
2018-09-21 23:54:25 -0700

    Insert Todo

--- Part1-Step1-NoSQL/lib/routes/routes-todos.ts
+++ Part1-Step1-NoSQL/lib/routes/routes-todos.ts
@@ -46,6 +46,41 @@ router.get('/:id', function (req, res : any) {
 
 })
 
+// insert Todo 
+router.post('/' , function (req : any, res : any) {
+
+    // console.log(req.body)
+    // console.log(req.body.moreInfo)
+
+    if (!req.body.title || !req.body.place || !req.body.description) {
+        res.t.message = "Invalid Request"
+        return res.send(res.t)
+    }
+
+    let todo = new Todos({
+        authorId: req.user._id,
+        title: req.body.title.trim(),
+        place: req.body.place.trim(),
+        description: req.body.description.trim(),
+    });
+
+    todo.save(function (err, todo:any) {
+
+        if (!todo) {
+            res.t.message = "Todo not available"
+            return res.send(res.t)
+        }
+
+        res.t.success = true
+        res.t.message = "Todo Added"
+        res.t.data = todo
+
+        return res.send(res.t)
+        
+    })
+
+})
+
 
 
 export default router
