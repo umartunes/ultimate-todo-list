ef2a8cb92a190a97de78dddf3482e85cc2167d37
AbdulRehman197 <abdulrehman19761@gmail.com>
2018-09-22 00:02:30 -0700

    DELETE test Fail

--- Part1-Step1-NoSQL/lib/test/api-todos.test.ts
+++ Part1-Step1-NoSQL/lib/test/api-todos.test.ts
@@ -21,31 +21,38 @@ describe("GET single todo test", () => {
 describe("POST single todo test", () => {
   it("should return 200 OK", () => {
     return request(server)
-    .post("/api/todos")
-    .send(
-      {
-        title: "testing",
-        palce:"Fsd",
-        discription: "at 1 Pm!"
-    })
+      .post("/api/todos")
+      .send(
+        {
+          title: "testing",
+          palce: "Fsd",
+          discription: "at 1 Pm!"
+        })
       .expect(200);
   });
   //Test update todo are coming or not
-describe("PUT single todo test", () => {
-  it("should return 200 OK", () => {
-    return request(server)
-    .put("/api/todos/5ba0b338622c260b9881c58e")
-    .send(
-      {
-        title: "testing",
-        palce:"Fsd",
-        discription: "at 1 Pm!"
-    })
-      .expect(200);
+  describe("PUT single todo test", () => {
+    it("should return 200 OK", () => {
+      return request(server)
+        .put("/api/todos/5ba0b338622c260b9881c58e")
+        .send(
+          {
+            title: "testing",
+            palce: "Fsd",
+            discription: "at 1 Pm!"
+          })
+        .expect(200);
+    });
   });
-});
 
-
+  //Test Delete todo are coming or not
+  describe("DELETE single todo test", () => {
+    it("should return 200", () => {
+      return request(server)
+        .delete("/api/todos/5ba0b338622c260b9881c58e")
+        .expect(200);
+    });
+  });
 });
 
 
