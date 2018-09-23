e0b9e8ed3aca23393a41b055ee2a6d3ba434d678
mubeenjan <mubeenjan770@yahoo.com>
2018-09-23 11:43:05 +0500

    Basic Structure of react Implemented

---
+++ Part2-Step4-NoSQL/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+
+import store from './config/store'
+
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+// Important Configurations Requirement
+// Global Configuration files must be imported first before any other file
+require( './config/global' )
+require( './config/axios' )
+// require( './config/firebase' )
+require( './config/fontawesome' )
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+)
+
+
+registerServiceWorker();
