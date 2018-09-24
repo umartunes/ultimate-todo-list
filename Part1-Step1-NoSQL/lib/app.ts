a6785462ee6e8fce3fffec778696ea8a7428f8ae
iffi Rana <iffirana708@gmail.com>
2018-09-24 15:58:50 +0500

    removed junk code

--- Part1-Step1-NoSQL/lib/app.ts
+++ Part1-Step1-NoSQL/lib/app.ts
@@ -51,32 +51,3 @@ class App {
 
 export default new App().server
 
-// const server = express()
-
-// //DB Connection
-// require('./config/db-config')
-
-// //Body Parser
-// server.use(bodyParser.urlencoded({ extended: true }))
-// server.use(bodyParser.json())
-
-
-// //Use Custom Middleware to get response template in all api routes
-// server.use( responseTemplate )
-
-// //Setting up routes
-// server.use('/api/todos', todosRouter)
-
-// server.get('/', (req, res: any) => {
-//     res.status(200).send("Root / Working...")
-// })
-
-// //Error Control
-// server.use(function (err, req, res, next) {
-
-//     console.error(err)
-//     res.status(500).send('Something broke!')
-
-// })
-
-// export default server
